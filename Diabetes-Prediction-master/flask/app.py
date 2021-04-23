99ef927c5346b6394fbce0775c677a486e763afc
karansinghthkur <38347103+karansinghthkur@users.noreply.github.com>
2021-04-23 19:54:51 +0530

    Update app.py

--- Diabetes-Prediction-master/flask/app.py
+++ Diabetes-Prediction-master/flask/app.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from flask import Flask, request, render_template
+import requests
 import pickle 
 import sklearn
 from sklearn.preprocessing import StandardScaler
@@ -39,4 +40,4 @@ def predict():
     return render_template('index.html', prediction_text='{}'.format(output))
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
